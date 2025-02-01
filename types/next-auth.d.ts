f3c92501ea948a1df9f00592e77bdf2e1ea3d5fe
DRBATA <164792895+DRBATA@users.noreply.github.com>
2025-02-01 15:51:04 +0000

    Update next-auth types with isSubscribed field

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,29 +1,27 @@
 import NextAuth from "next-auth"
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-      isSubscribed: boolean
-    }
+  interface User {
+    /**
+     * Make these optional so TypeScript won't complain if they don't exist.
+     */
+    id?: string
+    isSubscribed?: boolean
   }
 
-  interface User {
-    id: string
-    name?: string | null
-    email?: string | null
-    image?: string | null
-    isSubscribed: boolean
+  interface Session {
+    user?: User
   }
 }
 
 declare module "next-auth/jwt" {
+  /**
+   * This will ensure token can have these fields 
+   * without strict TypeScript errors.
+   */
   interface JWT {
-    id: string
-    isSubscribed: boolean
+    id?: string
+    isSubscribed?: boolean
   }
 }
 
